be8b7b3c8f5b1de98ce9dc79086fb057799e9ccf
Sashunator1984 <sashunator@gmail.com>
2025-08-19 11:36:02 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,3 @@
-# CREATE_USER_PATH хранит путь к API-методу для создания нового пользователя.
-# Этот путь будет использоваться для формирования полного URL-адреса в сочетании с базовым URL сервиса,
-# когда необходимо выполнить запрос на создание пользователя.
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER = "/api/v1/orders/track"
 URL_SERVICE = "https://1b677f36-34a0-4c54-9e57-c60f8c54f671.serverhub.praktikum-services.ru"
